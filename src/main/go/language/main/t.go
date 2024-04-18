8c1e8f745eb6331591904ce9a5a21d39e072f15c
andizhai <andizhai@tencent.com>
2024-04-18 21:29:58 +0800

    只保留最原始的 java 代码

--- src/main/go/language/main/t.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "github.com/AndiHappy/auxiliaryG/util"
-
-func main() {
-	var strSlice2 = []string{"India", "Canada", "Japan"}
-	util.PrintSliceMember(strSlice2)
-	util.PrintSlicePara(strSlice2)
-}
--- src/main/go/test_and_example/ini/ini.go
+++
@@ -1,6 +0,0 @@
-package main
-
-func main() {
-	//hello.Print()
-	//编译报错：./main.go:6:5: undefined: hello
-}
