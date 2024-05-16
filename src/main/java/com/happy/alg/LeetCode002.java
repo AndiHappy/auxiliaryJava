144d12e975537edd176ec6189cda611548a87244
andihappy <1015126672@qq.com>
2024-05-16 19:11:00 +0800

    how to tell people you know the answer

---
+++ src/main/java/com/happy/alg/LeetCode002.java
@@ -0,0 +1,36 @@
+package com.happy.alg;
+
+import com.happy.util.ListNode;
+
+public class LeetCode002 {
+
+    int carry=0;
+    public ListNode addTwoNumbers2(ListNode l1, ListNode l2){
+        if(l1 == null && l2 == null && carry ==0)  return null;
+        int sum = (l1!=null?l1.val:0) + (l2 != null?l2.val:0)+carry;
+        carry=sum/10;
+        return new ListNode(sum%10,addTwoNumbers(l1!=null?l1.next:null,l2!=null?l2.next:null));
+    }
+
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        // head 循环的过程中保持不变
+        ListNode head = new ListNode(-1);
+        // 变动的部分，另外的声明
+        ListNode cur = head;int carry=0;
+        while(l1 != null || l2 != null){
+            int l1value = l1==null?0:l1.val;
+            int l2value = l2==null?0:l2.val;
+            int curValue = (l1value+l2value+carry)%10;
+            carry=(l1value+l2value+carry)/10;
+            cur.next=new ListNode(curValue);
+            cur = cur.next;
+            l1 = l1==null?null:l1.next;
+            l2 = l2==null?null:l2.next;
+        }
+        if(carry>0){
+            cur.next=new ListNode(carry);
+        }
+        return head.next;
+    }
+}
---
+++ src/main/java/com/happy/alg/LeetCode003.java
@@ -0,0 +1,25 @@
+package com.happy.alg;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LeetCode003 {
+
+    public static int lengthOfLongestSubstring(String s) {
+        int result = 0;int from=0,end=0;
+        Map<Character,Integer> window = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char chari = s.charAt(i);
+            if(window.containsKey(chari)){
+                int repeatIndex = window.get(chari);
+                // abba second a index=0，but from=2
+                from=repeatIndex+1>from?repeatIndex+1:from;
+            }
+            end++;
+            result = Math.max(result,end-from);
+            window.put(chari,i);
+        }
+        return result;
+    }
+
+}
