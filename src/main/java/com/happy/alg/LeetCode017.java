a17d1b590204e839c9910f58d934426b725587aa
andihappy <andihappy5@gmail.com>
2025-09-13 20:56:26 +0800

    这里的关键在于处里数值的溢出问题

--- src/main/java/com/happy/alg/LeetCode017.java
+++ src/main/java/com/happy/alg/LeetCode017.java
@@ -1,9 +1,10 @@
 package com.happy.alg;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
-public class LeetCode017 {
+
     /*
     Given a string containing digits from 2-9 inclusive,
 
@@ -48,7 +49,36 @@ public class LeetCode017 {
     digits[i] is a digit in the range ['2', '9'].
     */
 
-    static String[] digitsCombina = new String[]{
+public class LeetCode017 {
+
+    class Solution2 {
+        String[] a = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        List<String> res = new LinkedList<>();
+        StringBuilder path = new StringBuilder();
+
+        public List<String> letterCombinations(String digits) {
+            if (digits.length() == 0) return res;
+            rt(digits, 0);
+            return res;
+        }
+
+        public void rt(String digits, int index) {
+            if (path.length() == digits.length()) {
+                res.add(path.toString());
+                return;
+            }
+
+            String s = a[digits.charAt(index) - '0'];
+            for (int i = 0; i < s.length(); i++) {
+                path.append(s.charAt(i));
+                rt(digits, index + 1);
+                path.deleteCharAt(path.length() - 1);
+            }
+
+        }
+    }
+
+    public static String[] digitsCombina = new String[]{
             "",
             "",
             "abc",
@@ -59,6 +89,54 @@ public class LeetCode017 {
             "pqrs",
             "tuv",
             "wxyz"};
+
+    class Solution {
+
+
+        public List<String> letterCombinations(String digits) {
+            List<String> result = new ArrayList<>();
+            // special case
+            if (digits == null || digits.length() == 0) return result;
+            return lc(digits, 0, digitsCombina,result);
+        }
+
+        /**
+         * @param digits input digits
+         * @param start  current index of digits
+         * @param digitsArray  mapping array
+         * @param res  result list
+         * @return  result list
+         */
+        private List<String> lc (String digits,int start,String[] digitsArray,List<String> res) {
+            if (start == digits.length() || start > digits.length()) return res;
+            List<String> tmpR = new ArrayList<>();
+            // find start
+            int index = digits.charAt(start) - '0'; //index of digitsCombina
+            String targetString = digitsArray[index];
+            if (res.isEmpty()) {
+                for (int i = 0; i < targetString.length(); i++) {
+                    tmpR.add(targetString.charAt(i) + "");
+                }
+            }else{
+                // res not empty
+                for (String s : res) {
+                    for (int i = 0; i < targetString.length(); i++) {
+                        tmpR.add(s + targetString.charAt(i));
+                    }
+                }
+            }
+            return lc(digits, start+1, digitsArray,tmpR);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        LeetCode017 leetCode017 = new LeetCode017();
+        LeetCode017.Solution s = leetCode017.new Solution();
+        System.out.println(s.letterCombinations("23"));
+        System.out.println("L1_30.LeetCode017.main");
+        System.out.println( letterCombinations("23"));
+    }
 
     public static List<String> letterCombinations(String digits) {
         List<String> result = new ArrayList<>();
@@ -86,10 +164,5 @@ public class LeetCode017 {
             return letterCombinations(digits,i+1,digitsCombina,tmpR);
         }
         return result;
-    }
-
-    public static void main(String[] args) {
-        System.out.println("L1_30.LeetCode017.main");
-        System.out.println( letterCombinations("23"));
     }
 }
--- src/main/java/com/happy/alg/LeetCode018.java
+++ src/main/java/com/happy/alg/LeetCode018.java
@@ -4,9 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class LeetCode018 {
-
-    //Given an array nums of n integers and an integer target, are there elements a,
+//Given an array nums of n integers and an integer target, are there elements a,
 // b, c, and d in nums such that a + b + c + d = target? Find all unique quadruplets
 // in the array which gives the sum of target.
 //
@@ -30,6 +28,121 @@ public class LeetCode018 {
 //
 // Related Topics Array Hash Table Two Pointers
 // 👍 2760 👎 382
+
+public class LeetCode018 {
+
+    public static void main(String[] args) {
+        System.out.println("keep happy");
+        LeetCode018 l = new LeetCode018();
+        LeetCode018.Solution2 s = l.new Solution2();
+        System.out.println(s.fourSum(new int[]{1,0,-1,0,-2,2},0));
+        System.out.println(s.fourSum(new int[]{0,0,0,1000000000,1000000000,1000000000,1000000000},1000000000));
+        System.out.println(fourSum(new int[]{-2,-1,-1,1,1,2,2},0));
+        System.out.println(fourSum(new int[]{1,-2,-5,-4,-3,3,3,5},-11));
+    }
+
+    class Solution2 {
+        public List<List<Integer>> fourSum(int[] nums, int target) {
+            List<List<Integer>> res = new ArrayList<List<Integer>>();
+            if (nums == null || nums.length < 4) {
+                return res;
+            }
+            Arrays.sort(nums);
+            int length = nums.length;
+            for (int i = 0; i < length - 3; i++) {
+                if (i > 0 && nums[i] == nums[i - 1]) {
+                    continue;
+                }
+                if ((long)nums[i]+nums[i + 1] +nums[i + 2]+ nums[i + 3]  > target) {
+                    break;
+                }
+                if ((long)nums[i]+nums[length - 3] + nums[length - 2] + nums[length - 1]  < target) {
+                    continue; // prevent integer overflow
+                }
+                for (int j = i + 1; j < length - 2; j++) {
+                    if (j > i + 1 && nums[j] == nums[j - 1]) {
+                        continue;
+                    }
+                    if ((long)nums[i]+nums[j] +nums[j + 1]+ nums[j + 2]  > target) {
+                        break;
+                    }
+                    if ((long)nums[i]+nums[j] + nums[length - 2] + nums[length - 1]  < target) {
+                        continue; // prevent integer overflow
+                    }
+                    int left = j + 1, right = length - 1;
+                    while (left < right) {
+                        long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+                        if (sum == target) {
+                            res.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                            while (left < right && nums[left] == nums[left + 1]) {
+                                left++;
+                            }
+                            left++;
+                            while (left < right && nums[right] == nums[right - 1]) {
+                                right--;
+                            }
+                            right--;
+                        } else if (sum < target) {
+                            left++;
+                        } else {
+                            right--;
+                        }
+                    }
+                }
+            }
+            return res;
+        }
+    }
+
+
+    class Solution {
+        public List<List<Integer>> fourSum(int[] nums, int target) {
+            List<List<Integer>> result = new ArrayList<>();
+            // special case
+            if (nums == null || nums.length < 4) return result;
+            Arrays.sort(nums); // sort first, then use two pointers
+            for (int i = 0; i < nums.length - 3; i++) {
+                if (nums[i] > target /4 )
+                    return result;
+                // skip same element
+                if (i > 0 && nums[i] == nums[i - 1]) continue;
+                for (int j = i + 1; j < nums.length - 2; j++) {
+                    // skip same elements
+                    if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+                    int m = j+1;int n = nums.length-1;
+                    while (m < n ) {
+                        int sum = nums[i] + nums[j] + nums[m] + nums[n];
+                        if (sum == target) {
+                            result.add(Arrays.asList(nums[i], nums[j], nums[m], nums[n]));
+                            m++;
+                            // nums[m] already be added, so move to next,and skip same elements
+                            while (m < n && nums[m] == nums[m - 1]) m++;
+                            n--;
+                            // nums[n] already be added, so move to next,and skip same elements
+                            while (m < n && nums[n] == nums[n + 1]) n--;
+                        } else if (sum < target) {
+                            m++;
+                            // skip same elements
+                            while (m < n && nums[m] == nums[m - 1]) m++;
+                        } else {
+                            n--;
+                            // skip same elements
+                            while (m < n && nums[n] == nums[n + 1]) n--;
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+    }
+
+
+
+
+
+
+
+
 
     // Notice that the solution set must not contain duplicate quadruplets.
     public static List<List<Integer>> fourSum(int[] nums, int target) {
@@ -67,16 +180,4 @@ public class LeetCode018 {
         }
         return result;
     }
-
-    public static void main(String[] args) {
-        System.out.println("keep happy");
-
-        System.out.println(fourSum(new int[]{1,0,-1,0,-2,2},0));
-
-        System.out.println(fourSum(new int[]{1,-2,-5,-4,-3,3,3,5},-11));
-
-
-    }
-
-
 }
